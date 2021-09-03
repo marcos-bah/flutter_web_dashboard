c93929c7fb2673faed65347b6b20a38233da36bc
marcos-bah <marcos.barbosa@unifei.edu.br>
2021-09-02 21:07:16 -0300

    [ADD] Adicionando auto fill

--- lib/pages/authentication/authentication.dart
+++ lib/pages/authentication/authentication.dart
@@ -26,120 +26,124 @@ class AuthenticationPage extends StatelessWidget {
           ),
           constraints: BoxConstraints(maxWidth: 300, maxHeight: 500),
           padding: EdgeInsets.all(24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(right: 12),
-                    child: Image.asset("assets/icons/logo.png"),
-                  ),
-                  Expanded(child: Container()),
-                ],
-              ),
-              SizedBox(height: 30),
-              Row(
-                children: [
-                  Text(
-                    "Login",
-                    style: GoogleFonts.roboto(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  CustomText(
-                    text: "Bem vindo de volta!",
-                    color: lightGrey,
-                  ),
-                ],
-              ),
-              SizedBox(height: 15),
-              FormCredentialInput(
-                label: "E-mail",
-                hint: "abc@domain.com",
-              ),
-              SizedBox(height: 15),
-              Obx(
-                () => FormCredentialInput(
-                  label: "Password",
-                  hint: "*******",
-                  obscure: !authController.isVisiblePass,
-                  suffix: IconButton(
-                    padding: EdgeInsets.only(right: 10),
-                    icon: Icon(
-                      !authController.isVisiblePass
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                      color: lightGrey,
-                    ),
-                    onPressed: authController.changedVisiblePass,
-                  ),
-                ),
-              ),
-              SizedBox(height: 15),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Checkbox(
-                        value: true,
-                        onChanged: (value) {},
-                      ),
-                      CustomText(
-                        text: "Lembrar",
-                        size: 14,
-                      ),
-                    ],
-                  ),
-                  CustomText(
-                    text: "Esqueceu a senha?",
-                    color: active,
-                    size: 14,
-                  ),
-                ],
-              ),
-              SizedBox(height: 15),
-              InkWell(
-                onTap: () {
-                  Get.offAllNamed(rootRoute);
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: active,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  alignment: Alignment.center,
-                  width: double.maxFinite,
-                  padding: EdgeInsets.symmetric(vertical: 16),
-                  child: CustomText(
-                    text: "Login",
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              SizedBox(height: 15),
-              RichText(
-                text: TextSpan(
+          child: AutofillGroup(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
                   children: [
-                    TextSpan(
-                      text: "Não possui uma conta? ",
+                    Padding(
+                      padding: EdgeInsets.only(right: 12),
+                      child: Image.asset("assets/icons/logo.png"),
                     ),
-                    TextSpan(
-                      text: "Sign up",
-                      style: TextStyle(
-                        color: active,
+                    Expanded(child: Container()),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Row(
+                  children: [
+                    Text(
+                      "Login",
+                      style: GoogleFonts.roboto(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
                       ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    CustomText(
+                      text: "Bem vindo de volta!",
+                      color: lightGrey,
                     ),
                   ],
                 ),
-              ),
-            ],
+                SizedBox(height: 15),
+                FormCredentialInput(
+                  label: "E-mail",
+                  hint: "abc@domain.com",
+                  fill: [AutofillHints.email],
+                ),
+                SizedBox(height: 15),
+                Obx(
+                  () => FormCredentialInput(
+                    label: "Password",
+                    hint: "*******",
+                    fill: [AutofillHints.password],
+                    obscure: !authController.isVisiblePass,
+                    suffix: IconButton(
+                      padding: EdgeInsets.only(right: 10),
+                      icon: Icon(
+                        !authController.isVisiblePass
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: lightGrey,
+                      ),
+                      onPressed: authController.changedVisiblePass,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: true,
+                          onChanged: (value) {},
+                        ),
+                        CustomText(
+                          text: "Lembrar",
+                          size: 14,
+                        ),
+                      ],
+                    ),
+                    CustomText(
+                      text: "Esqueceu a senha?",
+                      color: active,
+                      size: 14,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 15),
+                InkWell(
+                  onTap: () {
+                    Get.offAllNamed(rootRoute);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: active,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    alignment: Alignment.center,
+                    width: double.maxFinite,
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    child: CustomText(
+                      text: "Login",
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Não possui uma conta? ",
+                      ),
+                      TextSpan(
+                        text: "Sign up",
+                        style: TextStyle(
+                          color: active,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/pages/authentication/widgets/form_credential.dart
+++ lib/pages/authentication/widgets/form_credential.dart
@@ -4,6 +4,7 @@ class FormCredentialInput extends StatelessWidget {
   final String label;
   final String hint;
   final bool obscure;
+  final Iterable<String>? fill;
   final Widget? suffix;
   const FormCredentialInput({
     Key? key,
@@ -11,12 +12,14 @@ class FormCredentialInput extends StatelessWidget {
     required this.hint,
     this.obscure = false,
     this.suffix,
+    this.fill,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       obscureText: obscure,
+      autofillHints: fill,
       decoration: InputDecoration(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(20),
